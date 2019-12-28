60f474738717b7c2d247beaabe0ebad83d864195
Yuto Tokunaga <tokunaga@presto.phys.sci.osaka-u.ac.jp>
2019-12-28 19:53:45 +0900

    WIEN2k SO SCF

--- new/wien2k.qsub.sh
+++ new/wien2k.qsub.sh
@@ -17,8 +17,11 @@ case=$1
 
 # RUN FLAGS
 # =========
-run_wien2k_init=true
-run_wien2k_scf=true
+run_init=true
+run_scf=true
+run_band=true
+run_so_scf=true
+run_so_band=true
 
 # WIEN2k
 # ======
@@ -54,6 +57,16 @@ W2T=~/w2ktools/bin/w2t
 set -eu
 trap 'echo ERROR: $0:$LINENO exit $?' ERR INT
 
+ef () {
+  cat $case.scf | grep -E '^:FER' | tail -n1 | perl -pe 's/^[^=]+=\s*//'
+}
+
+scf_args=
+[[ -n "$ec" ]] && scf_args="$scf_args -ec $ec"
+[[ -n "$cc" ]] && scf_args="$scf_args -cc $cc"
+[[ -n "$n_iter" ]] && scf_args="$scf_args -i $n_iter"
+[[ -n "$in1new" ]] && scf_args="$scf_args -in1new $in1new"
+
 echo "==> WIEN2k for case: $case"
 
 # ディレクトリ名とCASEを合わせる
@@ -61,51 +74,84 @@ mkdir $case &> /dev/null || true
 cp $case.cif $case
 cd $case
 
-$run_wien2k_init && {
+if $run_init; then
   echo "--> generate master input $case.struct"
   $W2K/cif2struct $case.cif
 
-  echo "--> run wien2k init"
+  echo "--> run init"
   $W2K/init_lapw -b -red $rmt_red -vxc $vxc -ecut $ecut -rkmax $rkmax -mix $mix -numk $n_k
 
   echo -e "$n_k\n$shift_k" | $W2K/x kgen
-}
+fi
 
-$run_wien2k_scf && {
-  echo "--> run wien2k SCF"
-
-  args=
-  test -n "$ec" && args="$args -ec $ec"
-  test -n "$cc" && args="$args -cc $cc"
-  test -n "$n_iter" && args="$args -i $n_iter"
-  test -n "$in1new" && args="$args -in1new $in1new"
-
-  $W2K/run_lapw $args
+if $run_scf; then
+  echo "--> run SCF"
+  $W2K/run_lapw $scf_args
 
   echo -n "cp "; cp -v $case.dayfile $case.dayfile_scf
-}
+fi
 
-echo "--> calc band structure"
-# output: case.klist_band
-$W2T gen_klist_band $case $pearson $bz_syms $bz_div_n
+if $run_band; then
+  echo "--> run band structure calc"
+  # output: case.klist_band
+  $W2T gen_klist_band $case $pearson $bz_syms $bz_div_n
 
-# input: case.klist_band
-# output: case.scf1, case.energy
-$W2K/x lapw1 -band
+  # input: case.klist_band
+  # output: case.scf1, case.energy
+  $W2K/x lapw1 -band
+  # backup for restarting spaghetti
+  echo -n "cp "; cp -v $case.energy $case.energy_band
 
-ef=$(cat $case.scf | grep -E '^:FER' | tail -n1 | perl -pe 's/^[^=]+=\s*//')
-# backup for restarting spaghetti
-echo -n "cp "; cp -v $case.energy $case.energy_band
+  # update charges
+  $W2K/x lapw2 -qtl -band
 
-# update charges
-$W2K/x lapw2 -qtl -band
+  rm $case.insp 2> /dev/null || true
+  $W2K/x spaghetti || true # generate case.insp
+  sed -i $case.insp -e 's/0\.xxxx/'$(ef)'/'
+  $W2K/x spaghetti # generate case.spaghetti_ene
 
-$W2K/x spaghetti || true # generate case.insp
-sed -i $case.insp -e 's/0\.xxxx/'$ef'/'
-$W2K/x spaghetti # generate case.spaghetti_ene
+  # input: case.struct, case.klist_band, case.spaghetti_ene, case.qtl
+  # output: spaghetti.npz
+  $W2T spaghetti $case
+fi
 
-# input: case.struct, case.klist_band, case.scf1, case.energy_band, case.qtl
-# case.spaghetti_ene
-$W2T spaghetti $case
+if $run_so_scf; then
+  if [[ ! -f $case.inso ]]; then
+    echo "ERROR $case.inso not found"
+    echo "run initso to generate $case.inso"
+    exit 1
+  fi
+
+  echo -e "$n_k\n$shift_k" | $W2K/x kgen -so
+
+  echo "--> run SO SCF"
+  $W2K/run_lapw -so $scf_args
+fi
+
+if $run_so_band; then
+  echo "--> run SO band"
+  # output: case.klist_band
+  $W2T gen_klist_band $case $pearson $bz_syms $bz_div_n
+
+  # input: case.klist_band
+  # output: case.scf1, case.energy
+  $W2K/x lapw1 -band
+
+  # output: case.energyso
+  $W2K/x lapwso
+  echo -n "cp "; cp -v $case.energyso $case.energyso_band
+
+  # update charges
+  $W2K/x lapw2 -c -so -qtl -band
+
+  rm $case.insp 2> /dev/null || true
+  $W2K/x spaghetti || true # generate case.insp
+  sed -i $case.insp -e 's/0\.xxxx/'$(ef)'/'
+  $W2K/x spaghetti -so # generate case.spaghetti_ene
+
+  # input: case.struct, case.klist_band, case.spaghetti_ene, case.qtl
+  # output: spaghetti.npz
+  $W2T spaghetti $case
+fi
 
 echo "--> DONE (WIEN2k for case: $case)"
